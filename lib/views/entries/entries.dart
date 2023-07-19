8b9846e4b116ce14ac2ed2ca0017883175864639
mar1234yam <oumamimaryam49@gmail.com>
2023-07-20 00:40:51 +0100

    implemnting search for entries

--- lib/views/entries/entries.dart
+++ lib/views/entries/entries.dart
@@ -5,12 +5,13 @@ import 'package:get/get.dart';
 import '../../controllers/EntriesController.dart';
 import '../../widgets/CommandsList.dart';
 import '../../widgets/NewCommand.dart';
+
 import '../navigation_drawer.dart';
 
 class Entries extends GetView<EntriesController> {
   final BottomNavigationBar bottomNavigationBar;
-
-  const Entries({
+  final TextEditingController searchController = TextEditingController();
+  Entries({
     Key? key,
     required this.bottomNavigationBar,
   }) : super(key: key);
@@ -18,10 +19,21 @@ class Entries extends GetView<EntriesController> {
   @override
   Widget build(BuildContext context) {
     final EntriesController controller = Get.put(EntriesController());
+    void onSearchChanged() {
+      // Perform search based on the entered query
+      String query = searchController.text;
+      controller.filterCommands(query);
+      print("on changed");
+      // Implement search logic here
+    }
+
     return Scaffold(
       drawer: const NavigationDrawer(),
       appBar: AppBar(
-        title: const Text('Entries'),
+        title: SearchBar(
+          searchController: searchController,
+          onSearchChanged: controller.onSearchChanged,
+        ),
         backgroundColor: Colors.blue,
       ),
       bottomNavigationBar: bottomNavigationBar,
@@ -75,6 +87,77 @@ class Entries extends GetView<EntriesController> {
           ),*/
         ],
       ),
+    );
+  }
+}
+
+class SearchBar extends StatefulWidget {
+  final TextEditingController searchController;
+  final Function(String) onSearchChanged;
+
+  const SearchBar({
+    Key? key,
+    required this.searchController,
+    required this.onSearchChanged,
+  }) : super(key: key);
+
+  @override
+  State<SearchBar> createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar>
+    with SingleTickerProviderStateMixin {
+  bool _isActive = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        if (!_isActive)
+          Text("Entries Commands",
+              style: Theme.of(context).appBarTheme.titleTextStyle),
+        Expanded(
+          child: Align(
+            alignment: Alignment.centerRight,
+            child: AnimatedSize(
+              duration: const Duration(milliseconds: 250),
+              child: _isActive
+                  ? Container(
+                      width: double.infinity,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(4.0)),
+                      child: TextField(
+                        controller: widget.searchController,
+                        onChanged: widget.onSearchChanged,
+                        decoration: InputDecoration(
+                          hintText: 'Search for something',
+                          prefixIcon: const Icon(Icons.search),
+                          suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                _isActive = false;
+                                widget.searchController.clear();
+                                widget.onSearchChanged('');
+                              });
+                            },
+                            icon: const Icon(Icons.close),
+                          ),
+                        ),
+                      ),
+                    )
+                  : IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _isActive = true;
+                        });
+                      },
+                      icon: const Icon(Icons.search)),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
